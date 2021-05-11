53dabbe4b5381e155623304a42e0fc50e6c7bf12
Swetank Mohanty <swetank.mohanty@outlook.com>
2021-05-11 22:00:37 +0530

    Added a badge and minor improvements

--- README.md
+++ README.md
@@ -31,3 +31,5 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+[![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat-square)](http://makeapullrequest.com)
---
+++ src/app/models/ssl.ts
@@ -0,0 +1,35 @@
+import { Article } from "./base";
+
+export const SSL_RESOURCES: Array<Article> = [
+	{
+        id: 1,
+        title: 'Create and Install an Apache Self-Signed Certificate',
+        source: 'SSL Shopper',
+        url: 'https://www.sslshopper.com/article-how-to-create-and-install-an-apache-self-signed-certificate.html'
+    },
+	{
+        id: 2,
+        title: 'Creating your own SSL certificate authority',
+        source: 'DataCenter OverLords',
+        url: 'https://datacenteroverlords.com/2012/03/01/creating-your-own-ssl-certificate-authority/'
+    },
+	{
+        id: 3,
+        title: 'SSL Certificate Authority for local HTTPS development',
+        source: 'Delicious Brains',
+        url: 'https://deliciousbrains.com/ssl-certificate-authority-for-local-https-development/'
+    },
+	{
+        id: 4,
+        title: 'Creating self-signed SSL certificates with OpenSSL',
+        source: 'Dev Dungeon',
+        url: 'https://www.devdungeon.com/content/creating-self-signed-ssl-certificates-openssl'
+    },
+	{
+        id: 5,
+        title: 'Using SSL in Local Development',
+        source: 'Revath Kumar',
+        url: 'https://blog.revathskumar.com/2020/07/using-ssl-in-local-development.html'
+    }
+];
+
