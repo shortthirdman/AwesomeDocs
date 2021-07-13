6f788eccc9151bf35c95c99a90d6e153106552b2
Swetank Mohanty <swetank.mohanty@outlook.com>
2021-07-13 15:24:55 +0530

    [linux] Added publishing dates

--- src/app/models/linux.ts
+++ src/app/models/linux.ts
@@ -5,21 +5,21 @@ export const LINUX_RESOURCES: Array<Article> = [
         id: 1,
         title: 'Install Java SDK 1.8 on RHEL 7 & CentOS 7',
         source: 'Itz Geek',
-		published: null,
+		published: '2019-04-27',
         url: 'https://www.itzgeek.com/how-tos/linux/centos-how-tos/how-to-install-java-sdk-1-8-on-rhel-7-centos-7.html'
     },
 	{
         id: 2,
         title: 'Install Java 8 on CentOS, RHEL and Fedora',
-        source: 'Tech Admin',
-		published: null,
+        source: 'Tec Admin',
+		published: '2019-05-02',
         url: 'https://tecadmin.net/install-java-8-on-centos-rhel-and-fedora/'
     },
 	{
         id: 3,
         title: 'Top 8 YUM ThirdParty Repositories for CentOS and RHEL',
         source: 'Tecmint',
-		published: null,
+		published: '2018-09-14',
         url: 'https://www.tecmint.com/yum-thirdparty-repositories-for-centos-rhel/'
     },
 	{
@@ -33,7 +33,7 @@ export const LINUX_RESOURCES: Array<Article> = [
         id: 5,
         title: 'Backup Crontabs for users in Linux',
         source: 'Tec Admin',
-		published: null,
+		published: '2018-02-09',
         url: 'https://tecadmin.net/backup-crontabs-for-users-in-linux/'
     }
 ];
