b9fbea2ee4b434093573deee86f0b7406f180a23
Swetank Mohanty <shortthirdman@users.noreply.github.com>
2022-04-08 20:56:05 +0530

    [linux] Added more references and DIY guides for RedHat/CentOS/Ubuntu

--- src/app/models/linux.ts
+++ src/app/models/linux.ts
@@ -48,21 +48,69 @@ export const LINUX_RESOURCES: Array<Article> = [
 		published: '2021-07-08',
 		author: 'Josphat Mutai',
         url: 'https://computingforgeeks.com/how-to-install-postgresql-11-on-centos-rhel-8/'
-    }//,
-	// {
-        // id: 7,
-        // title: '',
-        // source: '',
-		// published: '',
-		// author: '',
-        // url: ''
-    // },
-	// {
-        // id: 8,
-        // title: '',
-        // source: '',
-		// published: '',
-		// author: '',
-        // url: ''
-    // }
+    },
+	{
+        id: 7,
+        title: 'PostfixAdmin – Create Virtual Mailboxes on CentOS 8/RHEL 8 Mail Server',
+        source: 'LinuxBabe',
+		published: '2021-10-31',
+		author: 'Xiao Guoan',
+        url: 'https://www.linuxbabe.com/redhat/postfixadmin-create-virtual-mailboxes-centos-mail-server'
+    },
+	{
+        id: 8,
+        title: 'Install Dovecot IMAP Server on CentOS 8/RHEL 8 & Enable TLS Encryption',
+        source: 'LinuxBabe',
+		published: '2021-12-25',
+		author: 'Xiao Guoan',
+        url: 'https://www.linuxbabe.com/redhat/install-dovecot-centos-enable-tls-encryption'
+    },
+	{
+        id: 9,
+        title: 'Let\'s Encrypt TLS/SSL Certificate with Nginx on Arch Linux Server',
+        source: 'LinuxBabe',
+		published: '2017-04-06',
+		author: 'Xiao Guoan',
+        url: 'https://www.linuxbabe.com/nginx/lets-encrypt-tlsssl-certificate-nginx-arch-linux-server'
+    },
+	{
+        id: 10,
+        title: 'Linux Security: Disable Root SSH Login on CentOS/Redhat',
+        source: 'LinuxBabe',
+		published: '2015-12-21',
+		author: 'Xiao Guoan',
+        url: 'https://www.linuxbabe.com/linux-server/linux-security-disable-root-ssh-login-on-centosredhat'
+    },
+	{
+        id: 11,
+        title: 'Linux Security: Control access to the su command',
+        source: 'LinuxBabe',
+		published: '2015-12-21',
+		author: 'Xiao Guoan',
+        url: 'https://www.linuxbabe.com/linux-server/linux-security-control-access-to-the-su-command'
+    },
+	{
+        id: 12,
+        title: 'Let\'s Encrypt Free TLS/SSL Certificate Renewal Errors',
+        source: 'LinuxBabe',
+		published: '2016-05-24',
+		author: 'Xiao Guoan',
+        url: 'https://www.linuxbabe.com/linux-server/lets-encrypt-free-tlsssl-certificate-renewal-errors'
+    },
+	{
+        id: 13,
+        title: 'Create Swap File on Cloud Linux Server to Prevent Out of Memory',
+        source: 'LinuxBabe',
+		published: '2021-11-21',
+		author: 'Xiao Guoan',
+        url: 'https://www.linuxbabe.com/linux-server/create-swap-file-linux-cloud-server'
+    },
+	{
+        id: 14,
+        title: '2 Simple Steps to Set up Passwordless SSH Login on Ubuntu',
+        source: 'LinuxBabe',
+		published: '2022-01-22',
+		author: 'Xiao Guoan',
+        url: 'https://www.linuxbabe.com/linux-server/setup-passwordless-ssh-login'
+    }
 ];
